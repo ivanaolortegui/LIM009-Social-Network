642614998e5afac5b73a51b2cd95cf2193e38f48
hellen08 <sccp.hvasquez@gmail.com>
2019-04-27 16:05:32 -0500

    'aceptando cambios'

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,30 +1,23 @@
-export default 'index!'
 // aqui exportaras las funciones que necesites
 
 /* export const myFunction = () => {
   // aqui tu codigo
 }
  */
-export function growl () {
-  return 'grrr';
-};
-
-export const type = 'Grizly';
-
-// let appFirebase = {};
-// const infoFirebase = () => {
-//   let config = {
-//     apiKey: "AIzaSyBrEkqM2kN4YhK8ALD4rGDziPene6zeWhQ",
-//     authDomain: "red-social-ecologica.firebaseapp.com",
-//     databaseURL: "https://red-social-ecologica.firebaseio.com",
-//     projectId: "red-social-ecologica",
-//     storageBucket: "red-social-ecologica.appspot.com",
-//     messagingSenderId: "246884214989"
-//   };
-//   firebase.initializeApp(config);
-//   appFirebase = firebase
-// }
-// infoFirebase();
+let appFirebase = {};
+const infoFirebase = () => {
+  let config = {
+    apiKey: "AIzaSyBrEkqM2kN4YhK8ALD4rGDziPene6zeWhQ",
+    authDomain: "red-social-ecologica.firebaseapp.com",
+    databaseURL: "https://red-social-ecologica.firebaseio.com",
+    projectId: "red-social-ecologica",
+    storageBucket: "red-social-ecologica.appspot.com",
+    messagingSenderId: "246884214989"
+  };
+  firebase.initializeApp(config);
+  appFirebase = firebase
+}
+infoFirebase();
 /* (function () {
   // Initialize Firebase
   var config = {
