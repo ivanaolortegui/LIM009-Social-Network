6d0dcf738b45623608777cca978dac823da7dc33
Hellend08 <sccp.hvasquez@gmail.com>
2019-04-30 15:41:15 -0500

    'name'

--- src/view/view-SignUp.js
+++ src/view/view-SignUp.js
@@ -13,14 +13,14 @@ export default () => {
   <input class="style-input" id="contraseña-login" type="password" placeholder="Ingrese contraseña">
   <br>
   <button class="button-registry" id="ingresar"> Log In </button>
-  <p>¿No tienes una cuenta? <button id="registro">Regístrate.</button></p>
+  <p>¿No tienes una cuenta? <button id="registry">Regístrate.</button></p>
   <h4>O inicia sesión con...</h4>
   <button id="Facebook"><img class="icon-facebook" src="./img/facebook.png"></button>
   <div id="container">
   </div>
 </section>`;
 body.innerHTML = template;
-const btnRegistro = body.querySelector('#registro');
+const btnRegistro = body.querySelector('#registry');
 btnRegistro.addEventListener('click',signInOnSubmit);
 const btnFacebook = body.querySelector('#Facebook');
 btnFacebook.addEventListener('click', facebookSubmit);
